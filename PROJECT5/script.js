87a4e204ab51ae32989fd656d4567eea4d4ce407
Chirag-Devda <devdachirag09@gmail.com>
2024-01-25 15:05:18 +0530

    add main folder

---
+++ PROJECT5/script.js
@@ -0,0 +1,16 @@
+// querySelectorAll
+//
+/* 
+let cittles = document.querySelectorAll(".feature_content");
+cittles[0].style.color = "red";
+cittles[1].style.color = "blue";
+cittles[2].style.color = "yellow";
+console.log(cittles); //selecting the element under class with there index
+*/
+//
+//
+// querySelector
+/*
+document.querySelector(".link_lists").style.background = "red"; //thes secelect the first one
+document.querySelector(".link_lists").style.color = "white"; //thes secelect the first one
+*/
