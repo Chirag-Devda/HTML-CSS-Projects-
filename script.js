920f74ed8739efcf4c1e5d49c62038b71415f7e7
Chirag-Devda <devdachirag09@gmail.com>
2024-03-01 20:33:45 +0530

    scripting for contact section

--- script.js
+++ script.js
@@ -39,3 +39,39 @@ document.querySelectorAll(".twitter").forEach((item) => {
     document.querySelector(".TwitterText").style.display = "block";
   });
 });
+
+// Media Query
+
+// Define a function that handles the media query
+function handleMaxWidth(x) {
+  if (x.matches) {
+    // If the viewport width is less than or equal to 700 pixels,
+    // change the background color to yellow
+    document.querySelectorAll(".toggle").forEach((item) => {
+      item.addEventListener("click", () => {
+        item.nextElementSibling.style.fontSize = "10px"; // Show the mobile number <p> element
+      });
+    });
+    document.querySelectorAll(".MobileText").forEach((item) => {
+      item.addEventListener("click", () => {
+        item.nextElementSibling.style.fontSize = "13px"; // Show the mobile number <p> element
+      });
+    });
+    document.querySelectorAll(".TwitterText").forEach((item) => {
+      item.addEventListener("click", () => {
+        item.nextElementSibling.style.fontSize = "9px"; // Show the mobile number <p> element
+      });
+    });
+  }
+}
+
+// Create a MediaQueryList object for a max-width of 700px
+var x = window.matchMedia("(max-width: 500px)");
+
+// Call the listener function initially
+handleMaxWidth(x);
+
+// Attach the listener function to state changes
+x.addEventListener("change", function () {
+  handleMaxWidth(x);
+});
